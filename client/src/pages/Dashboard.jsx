44fe1ea3eb006484a00ebe4d2c4c283f1ca93d3e
annaskhan-dev <kanas1431@gmail.com>
2026-08-21 16:47:21 +0500

    adding things

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1207,7 +1207,7 @@ export const Dashboard = ({ tickets: propTickets, onOpenCreateTicket }) => {
           <input
             type="text"
             placeholder="Search by Ticket ID or Title..."
-            className="w-full pl-10 pr-4 py-2 text-xs bg-slate-50/75 border border-slate-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500/20 focus:border-blue-600 transition-all shadow-2xs placeholder:text-slate-400 text-slate-800"
+            className="w-full pl-10 pr-4 py-2 text-xs bg-slate-50/75 border border-slate-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500/20 focus:border-blue-600 transition-all shadow-2xl placeholder:text-slate-400 text-slate-800"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
