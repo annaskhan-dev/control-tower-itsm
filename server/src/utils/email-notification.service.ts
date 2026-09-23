da4c120287878c190c786aeb86048f2792917d56
annaskhan-dev <kanas1431@gmail.com>
2026-09-23 21:56:48 +0500

    add email functionlaity

--- server/src/utils/email-notification.service.ts
+++ server/src/utils/email-notification.service.ts
@@ -11,6 +11,7 @@ export class EmailNotificationService {
       host: process.env.SMTP_HOST,
       port: parseInt(process.env.SMTP_PORT || '587', 10),
       secure: false, // true for 465, false for 587
+      requireTLS: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
