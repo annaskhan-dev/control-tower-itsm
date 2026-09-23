889cec63b52d1ceefdff64c3a405ed3cfc64efa1
annaskhan-dev <kanas1431@gmail.com>
2026-09-23 18:03:34 +0500

    add email functionlaity

--- server/src/tickets/tickets.module.ts
+++ server/src/tickets/tickets.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TicketsController } from './tickets.controller';
 import { TicketsService } from './tickets.service';
 import { TicketsGateway } from './tickets.gateway';
-import { EmailService } from '../services/emailService';
+import { EmailService } from '../utils/emailService';
 import { Ticket, TicketSchema } from './schemas/ticket.schema';
 import { SlaConfig, SlaConfigSchema } from './schemas/sla-config.schema';
 import { AuthModule } from '../auth/auth.module';
